b0a0e0e59a219348af47db48de34ad18817819f3
Tauqueer Khan <40416381+tkoriginal@users.noreply.github.com>
2019-02-27 18:43:39 -0500

    Completed ex 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,14 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+puts @store1.inspect
+puts @store2.inspect
+
+
+@store1.name = {name: "Newtons"}
+@store1.save
+
+puts @store1.inspect
